f674d739ecead2de9c63eff7d996d78b858f2ebf
Kenny Wolfenberger <kennywolfenberger@gmail.com>
2018-11-30 08:22:42 -0800

    Update to readme

--- FriendLetter/Startup.cs
+++ FriendLetter/Startup.cs
@@ -31,7 +31,7 @@ namespace FriendLetter
           name: "default",
           template: "{controller=Home}/{action=Index}/{id?}");
       });
-
+        app.UseStaticFiles();
         app.Run(async (context) =>
         {
           await context.Response.WriteAsync("Hello World!");
